292b03fa2e5fbab54d363b9cf91eeda245a835f6
Erik-TW <45933770+Erik-TW@users.noreply.github.com>
2021-06-07 19:44:43 +0200

    added calc funktion

--- index.js
+++ index.js
@@ -4,18 +4,27 @@ const playlists = require("./playlists.js");
 const dotenv = require('dotenv').config();
 const identifier = '$'
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-  client.user.setActivity(identifier + 'HELP', { type: 'WATCHING' });
+    client.user.setActivity(identifier + 'HELP', { type: 'WATCHING' });
 });
 
 client.on('message', msg => {
     const message = msg.content.toLowerCase();
     let index = 0;
-  switch(message) {
-      case identifier + 'rollchar':
-          msg.reply(rollChar());
-          break;
-        case identifier + 'help': 
+
+    //use this if chain if you have more logic than just if message exactly equals something
+    //don't forget to return so the switch case will not be run if an if statement is run. 
+    if (message.startsWith(identifier + "calc")) {
+        let start = identifier + "calc";
+        msg.reply(calc(message.substring(start.length).trim()));
+        return;
+    }
+
+    //use this switch for exact string matches
+    switch (message) {
+        case identifier + 'rollchar':
+            msg.reply(rollChar());
+            break;
+        case identifier + 'help':
             msg.reply(help());
             break;
         case identifier + 'forest':
@@ -42,21 +51,23 @@ client.on('message', msg => {
             index = Math.floor(Math.random() * (playlists.campfire.length));
             msg.channel.send(`/play ${playlists.campfire[index]}`);
             break;
-  }
+        case identifier + 'campfire':
+            index = Math.floor(Math.random() * (playlists.campfire.length));
+            msg.channel.send(`/play ${playlists.campfire[index]}`);
+            break;
+    }
 });
 
 const rollChar = () => {
     let totalAll = 0;
     let result = '';
     let diceArray = [];
-    for(let i = 0; i < 6; i++) {
-        for(let j = 0; j < 4; j++) {
-            let roll =  Math.floor(Math.random() * 6) + 1;
+    for (let i = 0; i < 6; i++) {
+        for (let j = 0; j < 4; j++) {
+            let roll = Math.floor(Math.random() * 6) + 1;
             diceArray.push(roll);
-        } 
-        console.log(diceArray);
+        }
         diceArray.sort();
-        console.log(diceArray);
         let total = diceArray[3] + diceArray[2] + diceArray[1];
         result += ` **${total}** : [ ${diceArray[3]}, ${diceArray[2]}, ${diceArray[1]} ] `
         diceArray = []
@@ -67,16 +78,54 @@ const rollChar = () => {
 }
 
 const help = () => {
-    return ' \n commands: \n ' + 
-    identifier + 'rollchar \n ' +
-    identifier + 'forest \n ' + 
-    identifier + 'city \n ' + 
-    identifier + 'combat \n '  + 
-    identifier + 'travel \n ' +
-    identifier + 'tavern \n ' +
-    identifier + 'campfire \n '
+    return ' \n commands: \n ' +
+        identifier + 'rollchar \n ' +
+        identifier + 'forest \n ' +
+        identifier + 'city \n ' +
+        identifier + 'combat \n ' +
+        identifier + 'travel \n ' +
+        identifier + 'tavern \n ' +
+        identifier + 'campfire \n ' +
+        identifier + 'calc [num1] [+,-,*,/] [num2] \n '
+}
 
-
+const calc = (input) => {
+    if (input.includes('+')) {
+        let separator = input.indexOf('+');
+        if (separator > 0) {
+            let num1 = parseInt(input.substring(0, separator).trim());
+            let num2 = parseInt(input.substring(separator + 1).trim());
+            return num1 + num2;
+        }
+    }
+    separator = input.indexOf('-');
+    if (separator > 0) {
+        let separator = input.indexOf('-');
+        if (separator > 0) {
+            let num1 = parseInt(input.substring(0, separator).trim());
+            let num2 = parseInt(input.substring(separator + 1).trim());
+            return num1 - num2;
+        }
+    }
+    separator = input.indexOf('/');
+    if (separator > 0) {
+        let separator = input.indexOf('/');
+        if (separator > 0) {
+            let num1 = parseInt(input.substring(0, separator).trim());
+            let num2 = parseInt(input.substring(separator + 1).trim());
+            return num1 / num2;
+        }
+    }
+    separator = input.indexOf('*');
+    if (separator > 0) {
+        let separator = input.indexOf('*');
+        if (separator > 0) {
+            let num1 = parseInt(input.substring(0, separator).trim());
+            let num2 = parseInt(input.substring(separator + 1).trim());
+            return num1 * num2;
+        }
+    }
+    return `Syntax error. check ${identifier}help for more information`;
 }
 
 client.login(process.env.DISCORD_KEY);
